31e61f959b0bcb8cb1b65ee1b45927139e84cf7a
kHAPPY2004 <happykamboj985@gmail.com>
2024-04-14 22:53:31 +0530

    successfully containerize the frontend and start app by root

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "http://backend:3000", // http://localhost:3000
     },
   },
   plugins: [react()],
