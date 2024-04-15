a4b12beeb143351c5b5ee187cb10c4eedeb25635
kHAPPY2004 <happykamboj985@gmail.com>
2024-04-15 20:08:04 +0530

    enhance routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,11 +20,30 @@ import { protectRoutePurchases } from "./store/atoms/userPurchases";
 import Video_play from "./courses/video_play";
 import Add_Video_Metadata from "./admin/add_video_metadata";
 
+interface ProtectedRouteCourseProps {
+  isAuthenticated: boolean;
+}
+function App() {
+  return (
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/new-courses" element={<New_Courses />} />
+          <Route path="/new-courses/:id" element={<New_Courses_slug />} />
+          <Route path="/add_course" element={<Add_Course />} />
+          <Route path="/add_course_content" element={<Add_Course_Content />} />
+          <Route path="/add_video_metadata" element={<Add_Video_Metadata />} />
+          <Route path="*" element={<ProtectedRouteUser />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
+  );
+}
+
 const ProtectedRouteUser = () => {
   // Check if user is authenticated
   const checkUserLoadable = useRecoilValueLoadable(checkUser);
-  const protrect = useRecoilValueLoadable(protectRoutePurchases);
-  let isAuthenticated;
 
   if (checkUserLoadable.state === "loading") {
     return (
@@ -35,22 +54,80 @@ const ProtectedRouteUser = () => {
   } else if (checkUserLoadable.state === "hasError") {
     return (
       <>
-        <div>Error while fetching data from backend</div>
+        <div>Error while fetching User information ...</div>
       </>
     );
   } else if (checkUserLoadable.state === "hasValue") {
-    isAuthenticated = checkUserLoadable.contents.data.success; // Assuming your checkUser atom returns a boolean
-    console.log("asqw", typeof isAuthenticated, isAuthenticated);
-    if (isAuthenticated) {
-      protrect.state === "hasValue" &&
-        console.log("protect123", protrect.contents, typeof protrect.contents);
-    }
+    const isAuthenticated: boolean = checkUserLoadable.contents.data.success; // Assuming your checkUser atom returns a boolean
+    return (
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/login"
+              element={<Navigate to="/dashboard" replace />}
+            />
+            <Route
+              path="/signup"
+              element={<Navigate to="/dashboard" replace />}
+            />
+            <Route
+              path="*"
+              element={
+                <ProtectedRouteCourse isAuthenticated={isAuthenticated} />
+              }
+            />
+          </>
+        ) : (
+          <>
+            <Route
+              path="/course/:id"
+              element={<Navigate to="/login" replace />}
+            />
+            <Route
+              path="/course/:id/:hash"
+              element={<Navigate to="/login" replace />}
+            />
+            <Route
+              path="/course/:id/:hash/:hash2"
+              element={<Navigate to="/login" replace />}
+            />
+            <Route
+              path="/dashboard"
+              element={<Navigate to="/login" replace />}
+            />
+            <Route
+              path="/login"
+              element={<Login completeUrl={location.pathname} />}
+            />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
+      </Routes>
+    );
   }
-
-  return (
-    <Routes>
-      {isAuthenticated ? (
+};
+const ProtectedRouteCourse: React.FC<ProtectedRouteCourseProps> = ({
+  isAuthenticated,
+}) => {
+  const protrect = useRecoilValueLoadable(protectRoutePurchases);
+  if (isAuthenticated) {
+    if (protrect.state === "loading") {
+      return (
         <>
+          <div>Loading...</div>
+        </>
+      );
+    } else if (protrect.state === "hasError") {
+      return (
+        <>
+          <div>Error while fetching courses ...</div>
+        </>
+      );
+    } else if (protrect.state === "hasValue") {
+      return (
+        <Routes>
           {protrect.contents ? (
             <>
               <Route path="/course/:id" element={<Course_slug />} />
@@ -73,57 +150,9 @@ const ProtectedRouteUser = () => {
               />
             </>
           )}
-
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<Navigate to="/dashboard" replace />} />
-          <Route
-            path="/signup"
-            element={<Navigate to="/dashboard" replace />}
-          />
-        </>
-      ) : (
-        <>
-          <Route
-            path="/course/:id"
-            element={<Navigate to="/login" replace />}
-          />
-          <Route
-            path="/course/:id/:hash"
-            element={<Navigate to="/login" replace />}
-          />
-          <Route
-            path="/course/:id/:hash/:hash2"
-            element={<Navigate to="/login" replace />}
-          />
-          <Route path="/dashboard" element={<Navigate to="/login" replace />} />
-          <Route
-            path="/login"
-            element={<Login completeUrl={location.pathname} />}
-          />
-          <Route path="/signup" element={<Signup />} />
-        </>
-      )}
-      <Route path="/" element={<Navbar />} />
-      <Route path="/new-courses" element={<New_Courses />} />
-      <Route path="/new-courses/:id" element={<New_Courses_slug />} />
-      {/* <Route path="/course/:id" element={<Course_slug />} /> */}
-      {/* <Route path="/course/:id/:hash" element={<Course_slug_Video />} /> */}
-      <Route path="/add_course" element={<Add_Course />} />
-      <Route path="/add_course_content" element={<Add_Course_Content />} />
-      <Route path="/add_video_metadata" element={<Add_Video_Metadata />} />
-      {/* <Route path="/course/:id/:hash/:hash2" element={<Video_play />} /> */}
-    </Routes>
-  );
+        </Routes>
+      );
+    }
+  }
 };
-
-function App() {
-  return (
-    <RecoilRoot>
-      <Router>
-        <ProtectedRouteUser />
-      </Router>
-    </RecoilRoot>
-  );
-}
-
 export default App;
